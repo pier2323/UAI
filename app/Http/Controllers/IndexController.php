00b5798713127cf40d2736cf2e23e11e4bdd7382
pier <pier44444@gmail.com>
2024-03-26 14:11:30 -0400

    acta tabla

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ActaEntrega;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
@@ -9,5 +10,11 @@ class IndexController extends Controller
     public function index()
     {
         return view('index');
+    }
+
+    public function dashboard()
+    {
+        $data = ActaEntrega::with('actuacionFiscal', 'personalEntrega.unidad')->get();
+        return view('dashboard', ['data' => $data]);
     }
 }
