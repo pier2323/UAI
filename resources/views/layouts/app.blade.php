6a7736a397e8f9a6a610ea6fdeb1dbe666911c93
Geferson Moreno <leonaikervan2001@gmail.com>
2024-04-04 15:54:15 -0400

    moviendo los links

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,45 +1,66 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+  <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+  <!-- Fonts -->
+  <link rel="preconnect" href="https://fonts.bunny.net">
+  <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Styles -->
-        @livewireStyles
-    </head>
-    <body class="font-sans antialiased">
-        <x-banner />
+  {{-- links styles --}}
+  <link rel="stylesheet" href="/css/app.css" />
+  <link rel="stylesheet" href="/css/styles.css" />
+  <link rel="stylesheet" href="/css/all.min.css" />
 
-        <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-menu')
+  {{-- links bootstrap --}}
+  <link href="/css/bootstrap/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="/css/dataTables.bootstrap5.min.css" />
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
+  <!-- Scripts -->
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+  <!-- Styles -->
+  @livewireStyles
+</head>
+
+<body class="font-sans antialiased">
+  <x-banner />
+
+  <div class="min-h-screen bg-gray-100">
+    @livewire('navigation-menu')
+
+    <!-- Page Heading -->
+    @if (isset($header))
+      <header class="bg-white shadow">
+        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+          {{ $header }}
         </div>
+      </header>
+    @endif
 
-        @stack('modals')
+    <!-- Page Content -->
+    <main>
+      {{ $slot }}
+    </main>
+  </div>
 
-        @livewireScripts
-    </body>
+  @stack('modals')
+
+  @livewireScripts
+  <!-- Bootstrap-->
+  <script src="js/bootstrap/bootstrap.bundle.min.js"></script>
+
+  <!-- jQuery -->
+  <script src="/js/jquery-dataTables/jquery.min.js"></script>
+  <!-- DataTable -->
+  <script src="/js/jquery-dataTables/jquery.dataTables.min.js"></script>
+  <script src="/js/jquery-dataTables/dataTables.bootstrap5.min.js"></script>
+</body>
+
 </html>
