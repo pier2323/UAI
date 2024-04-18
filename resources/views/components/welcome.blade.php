b6e46ed095c2f213ff24b4869c022de91930b8a3
pier <pier44444@gmail.com>
2024-04-18 16:19:28 -0400

    logo de sap_UAI , Mision y Vision  y implementacion

--- resources/views/components/welcome.blade.php
+++ resources/views/components/welcome.blade.php
@@ -1,16 +1,13 @@
-<div class="p-6 lg:p-8 bg-white border-b border-gray-200">
-    <x-application-logo class="block h-12 w-auto" />
+<style>
+    .contenedor {
+        display: flex;
+        justify-content: center;
+        justify-items: center;
+    }
+</style>
+<div class="contenedor">
+    <div class="p-6 lg:p-8 bg-white border-b border-gray-200">
+        <x-application-logo class="block h-12 w-auto" />
 
-    <h1 class="mt-8 text-2xl font-medium text-gray-900">
-        Bienvenido a La Unidad de Auditoria Interna
-    </h1>
-
-    <p class="mt-6 text-gray-500 leading-relaxed">
-
-
-        Mantente comunicado con acceso a una línea fija, para hacer o recibir llamadas a través de un par de cobre.
-
-        Podrás hacer o recibir llamadas locales, nacionales, internacionales y a móviles, además de contar con planes y servicios complementarios, con la ventaja de modalidad pospago.
-        
-    </p>
+    </div>
 </div>
