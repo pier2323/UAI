0a60929b746702405dd6e6e5d71c9905ff53f406
pier <pier44444@gmail.com>
2024-04-22 15:53:08 -0400

    se creo pag inicio e inclucion de imagenes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,88 +1,182 @@
 <x-app-layout>
-  <x-slot name="header">
-    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-      {{ __('Dashboard') }}
-    </h2>
-  </x-slot>
-  <div class="py-12">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-      <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-        <x-welcome />
-        <div class="px-4 py-2">
-
-          <div class="bg-gray-10 bg-opacity-10 grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8 p-6 lg:p-8">
-            <div class="container my-4 bg-white">
-              <div class="row">
-                <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
-                  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal"
-                    data-bs-whatever="@mdo">
-                    Nueva Acta
-                  </button>
-                  <div id="datatable_users_wrapper" class="dataTables_wrapper dt-bootstrap5 no-footer">
-                    <div class="col-sm-12">
-                      @include('modal')
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <x-welcome />
+                <div class="px-4 py-2">
+                    <div class="slide">
+                        <div class="slide-inner">
+                            <input class="slide-open" type="radio" id="slide-1" name="slide" aria-hidden="true"
+                                hidden="" checked="checked">
+                            <div class="slide-item">
+                                <img src="images/carrusel/3.jpg">
+                            </div>
+                            <input class="slide-open" type="radio" id="slide-2" name="slide" aria-hidden="true"
+                                hidden="">
+                            <div class="slide-item">
+                                <img src="images/carrusel/auditoria(4)(1).jpg">">
+                            </div>
+                            <input class="slide-open" type="radio" id="slide-3" name="slide" aria-hidden="true"
+                                hidden="">
+                            <div class="slide-item">
+                                <img
+                                    src="https://www.migueltroyano.com/wp-content/uploads/2020/09/excel_guardar_como_csv.jpg">
+                            </div>
+                            <label for="slide-3" class="slide-control prev control-1">‹</label>
+                            <label for="slide-2" class="slide-control next control-1">›</label>
+                            <label for="slide-1" class="slide-control prev control-2">‹</label>
+                            <label for="slide-3" class="slide-control next control-2">›</label>
+                            <label for="slide-2" class="slide-control prev control-3">‹</label>
+                            <label for="slide-1" class="slide-control next control-3">›</label>
+                            <ol class="slide-indicador">
+                                <li>
+                                    <label for="slide-1" class="slide-circulo">•</label>
+                                </li>
+                                <li>
+                                    <label for="slide-2" class="slide-circulo">•</label>
+                                </li>
+                                <li>
+                                    <label for="slide-3" class="slide-circulo">•</label>
+                                </li>
+                            </ol>
+                        </div>
                     </div>
-                  </div>
                 </div>
-              </div>
-
             </div>
-          </div>
-          <x-table>
-            <x-slot name="head">
-              <thead>
-
-                <tr>
-                  <th class="px-4 py-2 code centered sorting sorting_desc" tabindex="0"
-                    aria-controls="datatable_users" rowspan="1" colspan="1" {{-- style="width: 50px" --}}
-                    aria-label="#: activate to sort column ascending" aria-sort="descending">
-                    código
-                  </th>
-                  <th class="px-4 py-2 centered sorting" tabindex="0" aria-controls="datatable_users" rowspan="1"
-                    colspan="1" style="width: 189px" aria-label="Name: activate to sort column ascending">
-                    Descrición de la Actuacion
-                  </th>
-                  <th class="px-4 py-2 centered sorting" tabindex="0" aria-controls="datatable_users" rowspan="1"
-                    colspan="1" style="width: 166px" aria-label="Company: activate to sort column ascending">
-                    Fecha inicio
-                  </th>
-                  <th class="px-4 py-2 centered sorting_disabled" rowspan="1" colspan="1" style="width: 60px"
-                    aria-label="Status">
-                    Fecha fin
-                  </th>
-                  <th class="px-4 py-2 centered sorting_disabled" rowspan="1" colspan="1" style="width: 60px"
-                    aria-label="Status"> Area UAI
-                </tr>
-              </thead>
-            </x-slot>
-            @foreach ($data as $acta)
-              <tr class="hover:bg-gray-100">
-                <td class="px-4 py-2">{{ $acta->id }}</td>
-                <td class="px-4 py-2">{{ $acta->actuacionFiscal->objetivo }}</td>
-                <td class="px-4 py-2">{{ $acta->actuacionFiscal->inicio }}</td>
-                <td class="px-4 py-2">{{ $acta->actuacionFiscal->fin }}</td>
-                <td class="px-4 py-2">{{ $acta->personalEntrega->unidad->nombre }}</td>
-              </tr>
-            @endforeach
-          </x-table>
-          <livewire:counter />
+            <div class="botom">
+                <img src="/images/template/Actas.png" width="100px" height="100px">
+            </button>
+            
         </div>
-      </div>
     </div>
-  </div>
-   <div x-data="main()">
-    <button @click="incrementar">Incrementar</button>
-    <span x-text="contador"></span>
-</div>
-
-<script>
-    function main() {
-        return {
-            contador: 15,
-            incrementar: function() {
-                return this.contador++
-            }
+    <style>
+        .botom{
+            position: relative;
+            margin-top: 26px;
+            margin-left: 80px;
         }
-    }
-</script> 
+        .slide {
+            position: relative;
+            box-shadow: 0px 1px 6px rgba(0, 0, 0, 0.64);
+            margin-top: 26px;
+        }
+
+        .slide-inner {
+            position: relative;
+            overflow: hidden;
+            width: 100%;
+            height: calc(300px + 3em);
+        }
+
+        .slide-open:checked+.slide-item {
+            position: static;
+            opacity: 100;
+        }
+
+        .slide-item {
+            position: absolute;
+            opacity: 0;
+            -webkit-transition: opacity 0.6s ease-out;
+            transition: opacity 0.6s ease-out;
+        }
+
+        .slide-item img {
+            display: block;
+            height: auto;
+            max-width: 100%;
+        }
+
+        .slide-control {
+            background: rgba(0, 0, 0, 0.28);
+            border-radius: 50%;
+            color: #fff;
+            cursor: pointer;
+            display: none;
+            font-size: 40px;
+            height: 40px;
+            line-height: 35px;
+            position: absolute;
+            top: 50%;
+            -webkit-transform: translate(0, -50%);
+            cursor: pointer;
+            -ms-transform: translate(0, -50%);
+            transform: translate(0, -50%);
+            text-align: center;
+            width: 40px;
+            z-index: 10;
+        }
+
+        .slide-control.prev {
+            left: 2%;
+        }
+
+        .slide-control.next {
+            right: 2%;
+        }
+
+        .slide-control:hover {
+            background: rgba(0, 0, 0, 0.8);
+            color: #aaaaaa;
+        }
+
+        #slide-1:checked~.control-1,
+        #slide-2:checked~.control-2,
+        #slide-3:checked~.control-3 {
+            display: block;
+        }
+
+        .slide-indicador {
+            list-style: none;
+            margin: 0;
+            padding: 0;
+            position: absolute;
+            bottom: 2%;
+            left: 0;
+            right: 0;
+            text-align: center;
+            z-index: 10;
+        }
+
+        .slide-indicador li {
+            display: inline-block;
+            margin: 0 5px;
+        }
+
+        .slide-circulo {
+            color: #828282;
+            cursor: pointer;
+            display: block;
+            font-size: 35px;
+        }
+
+        .slide-circulo:hover {
+            color: #aaaaaa;
+        }
+
+        #slide-1:checked~.control-1~.slide-indicador li:nth-child(1) .slide-circulo,
+        #slide-2:checked~.control-2~.slide-indicador li:nth-child(2) .slide-circulo,
+        #slide-3:checked~.control-3~.slide-indicador li:nth-child(3) .slide-circulo {
+            color: #428bca;
+        }
+
+        #titulo {
+            width: 100%;
+            position: absolute;
+            padding: 0px;
+            margin: 0px auto;
+            text-align: center;
+            font-size: 27px;
+            color: rgba(255, 255, 255, 1);
+            font-family: 'Open Sans', sans-serif;
+            z-index: 9999;
+            text-shadow: 0px 1px 2px rgba(0, 0, 0, 0.33),
+                -1px 0px 2px rgba(255, 255, 255, 0);
+        }
+    </style>
+
 </x-app-layout>
