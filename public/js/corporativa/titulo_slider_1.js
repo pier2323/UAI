763c9f1abf2ca833e00973a8ce8927b6326bee77
pier <pier44444@gmail.com>
2024-05-02 16:12:40 -0400

    edit de la pagina de welcomen y agre los cdn

---
+++ public/js/corporativa/titulo_slider_1.js
@@ -0,0 +1,38 @@
+var sliderCounter = 0;
+var sliderContent = [
+    "Sistema Automatizado ",
+    "Unidad  Auditoria Interna",
+
+];
+var slider = document.querySelector("#slider");
+var sliderValue = document.querySelector("#sliderValue");
+
+function slide() {
+    if (sliderCounter >= sliderContent.length) {
+        sliderCounter = 0;
+    }
+
+    sliderValue.innerHTML = "";
+
+    sliderValue.classList.remove("holder-animation");
+    void sliderValue.offsetWidth;
+    sliderValue.classList.add("holder-animation");
+
+    for (i = 0; i < sliderContent[sliderCounter].length; i++) {
+        let letterDiv = document.createElement("div");
+        letterDiv.innerHTML = sliderContent[sliderCounter][i];
+
+        if (letterDiv.innerHTML == " ") {
+            letterDiv.innerHTML = "&nbsp;";
+        }
+        letterDiv.classList.add("start");
+        letterDiv.classList.add("animation");
+        letterDiv.style.animationDelay = i / 10 + "s";
+        sliderValue.appendChild(letterDiv);
+    }
+
+    sliderCounter++;
+}
+
+slide();
+setInterval(slide, 4000);
