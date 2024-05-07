1a4fe59d1110f138ca205e8984d0b56ee084a3bf
Geferson Moreno <leonaikervan2001@gmail.com>
2024-05-07 09:02:48 -0400

    designation

--- resources/views/designation/index.blade.php
+++ resources/views/designation/index.blade.php
@@ -5,27 +5,17 @@
   <link rel="stylesheet"
     href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,1,0" />
 
-  <form action="{{route('designation.download')}}" method="POST">
+
+  <form action="{{ route('designation.download') }}" method="POST">
     @csrf
     @method('POST')
 
-    <div x-data="{
-        inputs: 1,
-        addInput() {
-            this.inputs++;
-        },
-        removeInput() {
-            if (this.inputs > 1) {
-                this.inputs--;
-            }
-        }
-    }">
-
+    <div x-data="data()">
       <template x-for="(input, index) in Array.from({length: inputs})" :key="index">
         <div>
           <select name="auditor[]" :id="'input-' + index">
             @foreach ($personal as $person)
-              <option  value="{{ $person->id }}">{{ "$person->primer_nombre $person->primer_apellido" }}</option>
+              <option value="{{ $person->id }}">{{ "$person->primer_nombre $person->primer_apellido" }}</option>
             @endforeach
           </select>
           <select name="cargo[]" :id="'input-' + index">
@@ -39,7 +29,8 @@
         <div
           class="!z-5 shadow-3xl shadow-shadow-500 3xl:p-![18px] undefined relative flex flex w-full max-w-[300px] flex-col flex-col rounded-[20px] bg-white bg-white bg-clip-border !p-6 md:max-w-[400px]">
           <div class="mb-3 flex flex-col">
-            <button @click.prevent="addInput" class="rounded-l bg-gray-300 px-4 py-2 font-bold text-gray-800 hover:bg-gray-400">
+            <button @click.prevent="addInput"
+              class="rounded-l bg-gray-300 px-4 py-2 font-bold text-gray-800 hover:bg-gray-400">
               +
             </button>
             <button @click.prevent="removeInput"
@@ -57,26 +48,72 @@
         </div>
       </div>
     </div>
+    <div>
+      <label for="planificacion">planificacion</label>
+      <input type="date" name="planificacion" id="planificacion">
+    </div>
+    <div>
+      <label for="ejecucion">ejecucion</label>
+      <input type="date" name="ejecucion" id="ejecucion">
+    </div>
+    <div>
+      <label for="preeliminar">preeliminar</label>
+      <input type="date" name="preeliminar" id="preeliminar">
+    </div>
+    <div>
+      <label for="descargo">descargo</label>
+      <input type="date" name="descargo" id="descargo">
+    </div>
+    <div>
+      <label for="definitivo">definitivo</label>
+      <input type="date" name="definitivo" id="definitivo">
+    </div>
+
+
+
+
+
+
+
+
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+    <div class="text-center mt-20">
+        <lable class="text-lg font-semibold">Choose Date</lable> <br>
+        <input id="datepicker" class="border-2 border-gray-300 rounded px-3 py-2 w-56" type="text"
+            placeholder="Select a date">
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+    <script>
+        flatpickr("#datepicker", {
+            // Configuration options for Flatpickr
+            // You can customize the appearance and behavior here
+        });
+    </script>
+
+
+
+
+
+
+
+
+
   </form>
 
+  <script>
+    function data() {
+      return {
+        inputs: 1,
+        addInput() {
+          this.inputs++;
+        },
+        removeInput() {
+          if (this.inputs > 1) {
+            this.inputs--;
+          }
+        }
+      }
+    }
+  </script>
 </x-app-layout>
-
-{{-- <label for="email" class="text-navy-700 text-sm font-bold dark:text-white">Default</label>
-<input type="text" id="email" placeholder="@horizon.ui"
-class="mt-2 flex h-12 w-full items-center justify-center rounded-xl border border-gray-200 bg-white/0 p-3 text-sm outline-none"> --}}
-{{-- </div> --}}
-{{-- <div class="mb-3">
-  <label for="email2" class="text-navy-700 text-sm font-bold dark:text-white">Success</label>
-  <input type="text" id="email2" placeholder="Success input"
-  class="mt-2 flex h-12 w-full items-center justify-center rounded-xl border border-green-500 bg-white/0 p-3 text-sm text-green-500 outline-none placeholder:text-green-500 dark:!border-green-400 dark:!text-green-400 dark:placeholder:!text-green-400">
-</div> --}}
-{{-- <div class="mb-3">
-  <label for="email3" class="text-navy-700 text-sm font-bold dark:text-white">Error</label>
-  <input type="text" id="email3" placeholder="Error input"
-  class="mt-2 flex h-12 w-full items-center justify-center rounded-xl border border-red-500 bg-white/0 p-3 text-sm text-red-500 outline-none placeholder:text-red-500 dark:!border-red-400 dark:!text-red-400 dark:placeholder:!text-red-400">
-</div> --}}
-{{-- <div>
-  <label for="email4" class="text-navy-700 text-sm font-bold dark:text-white">Disabled</label>
-  <input disabled="" type="text" id="email4" placeholder="@horizon.ui"
-    class="mt-2 flex h-12 w-full cursor-not-allowed items-center justify-center rounded-xl border !border-none !bg-gray-100 bg-white/0 p-3 text-sm outline-none dark:!bg-white/5">
-  </div> --}}
